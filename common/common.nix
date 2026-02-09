39aa677c7c990d1e0daf88de6eba431554c138a4
Mathew Rupp <Rikairchy@users.noreply.github.com>
2026-02-09 17:16:12 -0500

    fix flake

--- common/common.nix
+++ common/common.nix
@@ -13,5 +13,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "25.11"; # Did you read the comment?
 }
--- flake.nix
+++ flake.nix
@@ -1,7 +1,15 @@
-outputs = { self, nixpkgs, ... }: {
-  nixosConfigurations = {
-    swarm1 = nixpkgs.lib.nixosSystem {
-      modules = [ ./hosts/swarm1/configuration.nix ];
+{
+  description = "HLS NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      swarm1 = nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/swarm1/configuration.nix ];
+      };
     };
   };
-};
+}
