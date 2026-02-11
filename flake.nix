8d5574c994facaea0ef10867779edbbb8c500dbf
Mathew Rupp <Rikairchy@users.noreply.github.com>
2026-02-11 16:48:33 -0500

    add new hosts to flake.nix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,17 @@
       swarm1 = nixpkgs.lib.nixosSystem {
         modules = [ ./hosts/swarm1/configuration.nix ];
       };
+      swarm2 = nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/swarm2/configuration.nix ];
+      };
+      swarm3 = nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/swarm3/configuration.nix ];
+      };
+      swarm4 = nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/swarm4/configuration.nix ];
+      };
+      swarm5 = nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/swarm5/configuration.nix ];
     };
   };
 }
